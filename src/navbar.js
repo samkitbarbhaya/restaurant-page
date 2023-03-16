895360ca1bfd9b171c9819ea0deffdddeb0b3f7c
Samkit Barbhaya <samkitbarbhaya@gmail.com>
2023-03-15 18:14:28 -0700

    Added Navbar Logic in Navbar.JS

---
+++ src/navbar.js
@@ -0,0 +1,16 @@
+
+function render() {
+    const navItems = ["HOME","MENU","CONTACT"]
+    const $ul = document.createElement("ul")
+    navItems.forEach((item)=>{
+        const $li = document.createElement("li")
+        $li.textContent = item
+        $ul.appendChild($li)
+    })
+    const $nav = document.createElement("nav")
+    $nav.className = "navbar"
+    $nav.appendChild($ul)
+    $content.appendChild($nav);
+}
+
+export { render as renderNav};
