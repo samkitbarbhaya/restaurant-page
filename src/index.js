17fc9e663145d62606615c46f6893f44113a30c2
Samkit Barbhaya <samkitbarbhaya@gmail.com>
2023-03-15 18:26:46 -0700

    Main Home JS

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { renderNav } from "./navbar";
+import { renderMainHome } from "./main-home";
+
+const $content = document.getElementById("content");
---
+++ src/main-home.js
@@ -0,0 +1,29 @@
+
+function render() {
+
+    const $pWelcome = document.createElement("p")
+    $pWelcome.className = "cursive golden"
+    $pWelcome.textContent = "Welcome"
+
+    const $h1 = document.createElement("h1")
+    $h1.textContent = "LA GATEAU"
+
+    const $hr = document.createElement("hr")
+
+    const $pDescription = document.createElement("p")
+    $pDescription.className = "text-center white"
+    $pDescription.textContent = "The pleasure of eating with your hands. Good land!"
+
+    const $button = document.createElement("button")
+    $button.textContent = "SEE THE MENU"
+
+    const $div = document.createElement("div")
+    $div.className = "main-content"
+    $div.append($pWelcome, $h1, $hr, $pDescription, $button)
+
+    const $main = document.createElement("main")
+    $main.className = "main"
+    $main.appendChild($div)
+}
+
+export {render as renderMainHome}
