14aec62c479ab39125b10960079ed1acfd5303ee
Samkit Barbhaya <samkitbarbhaya@gmail.com>
2023-03-15 18:34:34 -0700

    Added footer logic in the JS File removed everthing from the HTML File

---
+++ src/footer.js
@@ -0,0 +1,14 @@
+import { $content } from "./index"
+
+function render() {
+    const $footer = document.createElement("footer")
+    $footer.className = "footer"
+
+    const $p = document.createElement("p")
+    $p.textContent = "Developed by Samkit Barbhaya"
+
+    $footer.appendChild($p)
+    $content.appendChild($footer)
+}
+
+export { render as renderFooter};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { renderNav } from "./navbar";
 import { renderMainHome } from "./main-home";
+import { renderFooter } from "./footer";
 
 const $content = document.getElementById("content");
 
 function render(){
     renderNav()
     renderMainHome()
+    renderFooter()
 }
 
 render();
